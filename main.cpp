48edfe76f28676547a664b3d5510d6af3eccf353
Titrom25 <titrom25@mail.ru>
2022-04-21 17:46:49 +0700

    Improve text file reading and ngrams searching performance

--- main.cpp
+++ main.cpp
@@ -5,19 +5,22 @@
 #include <codecvt>
 #include <fstream>
 #include <sys/mman.h>
-
 #include "dictionary.h"
 #include "filemap.h"
 #include "WordContext.h"
+#include <chrono>
 
 using namespace std;
 using recursive_directory_iterator = std::__fs::filesystem::recursive_directory_iterator;
 
+int OUTPUT_COUNT = 50;
 int COUNT_THRESHOLD = 2;
 float STABILITY = 0.1;
 
 unordered_map <wstring, vector<WordContext*>> leftExt;
 unordered_map <wstring, vector<WordContext*>> rightExt;
+unordered_map <string, vector<int>> newNGramsPositions;
+unordered_map <string, vector<int>> oldNGramsPositions;
 
 vector<string> getFilesFromDir(const string& dirPath) {
     vector<string> files;
@@ -27,44 +30,29 @@ vector<string> getFilesFromDir(const string& dirPath) {
     return files;
 }
 
-WordContext* processContext(const vector<wstring>& stringContext, int windowSize,
-                            unordered_map <wstring, vector<Word*>> *dictionary,
-                            unordered_map <wstring, WordContext*> *NGrams
-                            ) {
-    auto &f = std::use_facet<std::ctype<wchar_t>>(std::locale());
-
-    vector<Word *> contextVector;
-
-    for (int i = 0; i < windowSize; i++) {
-        wstring contextWord = stringContext.at(i);
-        f.toupper(&contextWord[0], &contextWord[0] + contextWord.size());
-
-        if (dictionary->find(contextWord) == dictionary->end()) {
-            Word *newWord = new Word();
-            newWord->word = contextWord;
-            newWord->partOfSpeech = L"UNKW";
-            dictionary->emplace(newWord->word, vector<Word *>{newWord});
-        }
-
-        vector<Word *> &words = dictionary->at(contextWord);
-        contextVector.push_back(words.at(0));
-    }
-
+WordContext* processContext(const vector<Word*>& wordVector, int windowSize,
+                            unordered_map <wstring, WordContext*> *NGrams,
+                            bool isNotExt) {
     wstring normalizedForm;
+    normalizedForm.reserve(windowSize * 8);
     wstring leftNormalizedExt;
+    leftNormalizedExt.reserve(windowSize * 8);
     wstring rightNormalizedExt;
+    rightNormalizedExt.reserve(windowSize * 8);
 
-    for (int i = 0; i < contextVector.size(); i++) {
-        Word* word = contextVector[i];
+    vector<Word*> contextVector;
+    for (int i = 0; i < windowSize; i++) {
+        Word* word = wordVector[i];
+        contextVector.push_back(word);
         if (i == 0) {
             rightNormalizedExt += word->word + L" ";
-        } else if (i == contextVector.size() - 1) {
+        } else if (i == wordVector.size() - 1) {
             leftNormalizedExt += word->word + L" ";
         } else {
             leftNormalizedExt += word->word + L" ";
             rightNormalizedExt += word->word + L" ";
         }
-        normalizedForm += word->word + L" ";
+        normalizedForm += word->word + L' ';
     }
 
     leftNormalizedExt.pop_back();
@@ -99,67 +87,118 @@ void addContextToSet(WordContext* context, unordered_map <wstring, WordContext*>
     ngram->textEntry.insert(filePath);
 }
 
-void handleWord(const wstring& wordStr,
-                unordered_map <wstring, vector<Word*>> *dictionary,
-                vector<wstring>* leftStringNGrams,
+void handleWord(Word* word,
+                vector<Word*>* leftWordContext,
                 unordered_map <wstring, WordContext*> *NGrams,
-                int windowSize,
-                const string& filePath) {
+                int windowSize, const string& filePath,
+                const int wordPosition) {
     for (int i = windowSize; i <= windowSize + 1; i++) {
-        if (i <= leftStringNGrams->size()) {
-            WordContext* phraseContext = processContext(*leftStringNGrams, i, dictionary, NGrams);
-            if (phraseContext != nullptr)
+        if (i <= leftWordContext->size()) {
+            bool isCloseToNGramm = false;
+            if (windowSize > 2) {
+                if (oldNGramsPositions.find(filePath) == newNGramsPositions.end())
+                    continue;
+                vector<int> positions = oldNGramsPositions.at(filePath);
+                for (int position : positions)
+                    if (abs(wordPosition - position) <= 0) {
+                        isCloseToNGramm = true;
+                        break;
+                    }
+            } else
+                isCloseToNGramm = true;
+
+            if (!isCloseToNGramm)
+                continue;
+
+            WordContext* phraseContext = processContext(*leftWordContext, i, NGrams, i == windowSize);
+            if (phraseContext != nullptr) {
                 addContextToSet(phraseContext, NGrams, filePath);
-        }
-    }
-    if (leftStringNGrams->size() == (windowSize + 1)) {
-        leftStringNGrams->erase(leftStringNGrams->begin());
-    }
-
-    leftStringNGrams->push_back(wordStr);
-//    wcout << endl;
-}
-
-void handleFile(const string& filePath, unordered_map <wstring, vector<Word*>> *dictionary,
-                unordered_map <wstring, WordContext*> *NGrams, int windowSize) {
-
-    size_t length;
-    auto filePtr = map_file(filePath.c_str(), length);
-    auto firstChar = filePtr;
-    auto lastChar = filePtr + length;
-
-    vector<wstring> leftStringNGrams;
-
-    while (filePtr && filePtr != lastChar) {
-        auto stringBegin = filePtr;
-        filePtr = static_cast<char *>(memchr(filePtr, '\n', lastChar - filePtr));
-
-        wstring line = wstring_convert<codecvt_utf8<wchar_t>>().from_bytes(stringBegin, filePtr);
-        wstring const delims {L" :;.,!?()" };
-
-        size_t beg, pos = 0;
-        while ((beg = line.find_first_not_of(delims, pos)) != string::npos) {
-            pos = line.find_first_of(delims, beg + 1);
-            wstring wordStr = line.substr(beg, pos - beg);
-
-            handleWord(wordStr, dictionary,
-                       &leftStringNGrams,
-                       NGrams, windowSize, filePath);
-
-            if (line[pos] == L'.' || line[pos] == L',' ||
-                line[pos] == L'!' || line[pos] == L'?') {
-                wstring punct;
-                punct.push_back(line[pos]);
-                handleWord(punct, dictionary,
-                           &leftStringNGrams,
-                           NGrams, windowSize, filePath);
+                if (newNGramsPositions.find(filePath) == newNGramsPositions.end())
+                    newNGramsPositions.emplace(filePath, vector<int>{});
+                newNGramsPositions.at(filePath).push_back(wordPosition);
             }
         }
-        if (filePtr)
-            filePtr++;
+    }
+    if (leftWordContext->size() == (windowSize + 1)) {
+        leftWordContext->erase(leftWordContext->begin());
     }
 
-    munmap(firstChar, length);
+    leftWordContext->push_back(word);
+}
+
+void handleFile(const string& filepath, const vector<Word*>& fileContent, unordered_map <wstring, vector<Word*>> *dictionary,
+                unordered_map <wstring, WordContext*> *NGrams, int windowSize) {
+    vector<Word*> leftWordContext;
+
+    int wordPosition = 0;
+    for (Word* word : fileContent) {
+        handleWord(word,&leftWordContext,
+                   NGrams, windowSize, filepath, wordPosition);
+        wordPosition++;
+    }
+}
+
+unordered_map <string, vector<Word*>> readAllTexts(const vector<string>& files,
+                                                   unordered_map <wstring, vector<Word*>> *dictionary) {
+    auto &f = std::use_facet<std::ctype<wchar_t>>(std::locale());
+    unordered_map <string, vector<Word*>> fileContents;
+    vector<wstring> leftStringNGrams;
+
+    for (const auto& filepath : files) {
+        fileContents.emplace(filepath, vector<Word *>{});
+        vector<Word*> fileContent = fileContents.at(filepath);
+
+        size_t length;
+        auto filePtr = map_file(filepath.c_str(), length);
+        auto firstChar = filePtr;
+        auto lastChar = filePtr + length;
+
+        while (filePtr && filePtr != lastChar) {
+            auto stringBegin = filePtr;
+            filePtr = static_cast<char *>(memchr(filePtr, '\n', lastChar - filePtr));
+
+            wstring line = wstring_convert<codecvt_utf8<wchar_t>>().from_bytes(stringBegin, filePtr);
+            wstring const delims{L" :;.,!?() \r\n"};
+
+            size_t beg, pos = 0;
+            while ((beg = line.find_first_not_of(delims, pos)) != string::npos) {
+                pos = line.find_first_of(delims, beg + 1);
+                wstring wordStr = line.substr(beg, pos - beg);
+
+                f.toupper(&wordStr[0], &wordStr[0] + wordStr.size());
+
+                if (dictionary->find(wordStr) == dictionary->end()) {
+                    Word *newWord = new Word();
+                    newWord->word = wordStr;
+                    newWord->partOfSpeech = L"UNKW";
+                    dictionary->emplace(newWord->word, vector<Word *>{newWord});
+                }
+
+                vector<Word *> &words = dictionary->at(wordStr);
+                fileContent.push_back(words.at(0));
+
+                if (line[pos] == L'.' || line[pos] == L',' ||
+                    line[pos] == L'!' || line[pos] == L'?') {
+                    wstring punct;
+                    punct.push_back(line[pos]);
+                    if (dictionary->find(punct) == dictionary->end()) {
+                        Word *newWord = new Word();
+                        newWord->word = punct;
+                        newWord->partOfSpeech = L"UNKW";
+                        dictionary->emplace(newWord->word, vector<Word *>{newWord});
+                    }
+
+                    vector<Word *> &wordsPunct = dictionary->at(punct);
+                    fileContent.push_back(wordsPunct.at(0));
+                }
+            }
+            if (filePtr)
+                filePtr++;
+        }
+        munmap(firstChar, length);
+        fileContents.at(filepath) = fileContent;
+    }
+    return fileContents;
 }
 
 void findNGrams(const string& dictPath, const string& corpusPath,
@@ -170,49 +209,55 @@ void findNGrams(const string& dictPath, const string& corpusPath,
     unordered_map<wstring, WordContext *> NGrams;
     vector<WordContext *> stableNGrams;
     cout << "Start handling files\n";
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    unordered_map <string, vector<Word*>> filesContent = readAllTexts(files, &dictionary);
 
     int windowSize = 2;
     while (true) {
-        wcout << "Window size: " << windowSize << endl;
-        for (int i = 0; i < 1; i++) {
-            for (const string &file: files)
-                handleFile(file, &dictionary, &NGrams, windowSize);
-
-            unordered_map<wstring, WordContext *> thresholdedNGrams;
-
-            for (auto &pair: NGrams) {
-                if (pair.second->totalEntryCount >= COUNT_THRESHOLD) {
-                    try {
-                        if (pair.second->words.size() != windowSize)
-                            continue;
-
-                        int extensionMax = 0;
-
-                        vector<WordContext *> leftWordContexts = leftExt.at(pair.second->normalizedForm);
-                        vector<WordContext *> rightWordContexts = rightExt.at(pair.second->normalizedForm);
-
-                        for (WordContext *wc: leftWordContexts)
-                            if (wc->totalEntryCount > extensionMax)
-                                extensionMax = wc->totalEntryCount;
-
-                        for (WordContext *wc: rightWordContexts)
-                            if (wc->totalEntryCount > extensionMax)
-                                extensionMax = wc->totalEntryCount;
-
-                        pair.second->stability = (extensionMax / (float) pair.second->totalEntryCount);
-                        if (pair.second->stability <= STABILITY)
-                            stableNGrams.push_back(pair.second);
-
-                        thresholdedNGrams.emplace(pair);
-
-                    } catch (const out_of_range &) {}
-                }
-            }
-            wcout << NGrams.size() << " | " << thresholdedNGrams.size() << endl;
-            NGrams = thresholdedNGrams;
+        for (const string &filepath: files) {
+            handleFile(filepath, filesContent.at(filepath), &dictionary, &NGrams, windowSize);
         }
-//        break;
+
+        unordered_map<wstring, WordContext *> thresholdedNGrams;
+
+        for (auto &pair: NGrams) {
+            if (pair.second->totalEntryCount >= COUNT_THRESHOLD) {
+                try {
+                    if (pair.second->words.size() != windowSize)
+                        continue;
+
+                    int extensionMax = 0;
+
+                    vector<WordContext *> leftWordContexts = leftExt.at(pair.second->normalizedForm);
+                    vector<WordContext *> rightWordContexts = rightExt.at(pair.second->normalizedForm);
+
+                    for (WordContext *wc: leftWordContexts) {
+                        if ((wc->words.size() == pair.second->words.size() + 1) && wc->totalEntryCount > extensionMax)
+                            extensionMax = wc->totalEntryCount;
+                    }
+
+                    for (WordContext *wc: rightWordContexts) {
+                        if ((wc->words.size() == pair.second->words.size() + 1) && wc->totalEntryCount > extensionMax)
+                            extensionMax = wc->totalEntryCount;
+                    }
+
+                    pair.second->stability = ((float) extensionMax / (float) pair.second->totalEntryCount);
+                    if (pair.second->stability <= STABILITY)
+                        stableNGrams.push_back(pair.second);
+
+                    thresholdedNGrams.emplace(pair);
+                } catch (const out_of_range &) {}
+            }
+        }
+        wcout << "Window size: " << windowSize << ", repeated ngrams: " << thresholdedNGrams.size() << endl;
+        NGrams = thresholdedNGrams;
+
+        oldNGramsPositions = newNGramsPositions;
+        newNGramsPositions.clear();
         windowSize++;
+        if (NGrams.empty())
+            break;
     }
 
     struct {
@@ -223,26 +268,31 @@ void findNGrams(const string& dictPath, const string& corpusPath,
     ofstream outFile (outputFile.c_str());
 
     cout << "\n";
+    int printCount = 0;
     for (const WordContext* context : stableNGrams) {
         string line = wstring_convert<codecvt_utf8<wchar_t>>().to_bytes(context->normalizedForm);
-//        outFile << "<\"" << line << "\", " << context->totalEntryCount << ">\n";
-//
-//        wcout << "<\"" << context->normalizedForm
-//        << "\", Total entries: " << context->totalEntryCount
-//        << ", TF: " << context->textEntry.size()
-//        << ", Stability: " << context->stability
-//        << ">\n";
+        outFile << "<\"" << line << "\", " << context->totalEntryCount << ">\n";
+
+        wcout << "<\"" << context->normalizedForm
+        << "\", Total entries: " << context->totalEntryCount
+        << ", TF: " << context->textEntry.size()
+        << ", Stability: " << context->stability
+        << ">\n";
+
+        printCount++;
+        if (printCount >= OUTPUT_COUNT)
+            break;
     }
 
     outFile.close();
     cout << "End handling files\n";
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
 }
 
-#include <chrono>
-#include <thread>
-
 int main() {
-    STABILITY = 0.1;
+    OUTPUT_COUNT = 50;
+    STABILITY = 0.5;
 
     locale::global(locale("ru_RU.UTF-8"));
     wcout.imbue(locale("ru_RU.UTF-8"));
@@ -252,6 +302,5 @@ int main() {
     string outputFile = "ngrams.txt";
 
     findNGrams(dictPath, corpusPath, outputFile);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100000));
     return 0;
 }
